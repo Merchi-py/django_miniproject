1c5ffadd821d2bbd578353b2b02a0298b7acefc1
Svyat <svyat.kostevich@gmail.com>
2025-12-10 21:38:48 +0100

    feat(order.js): Notifcation Modal and form reset

--- mini_project/course/static/js/order.js
+++ mini_project/course/static/js/order.js
@@ -1,18 +1,22 @@
-$('#orderCreate').submit(function (event){
+$('#orderCreate').submit(function (event) {
     event.preventDefault();
     let form = $(this);
     let data = {};
+
     $.each($(form).serializeArray(), function(index, field) {
         data[field.name] = field.value;
     });
+
     $.ajax(form.attr('action'), {
         'type': 'POST',
         'async': true,
         'dataType': 'json',
         'data': data,
-        'success': function (response){
+        'success': function (response) {
             $('#OrderCreateModal').modal('hide');
+            $('#customAlert').modal('show');
+
+            form[0].reset(); // reset every inputs in the form
         },
     });
 });
-
