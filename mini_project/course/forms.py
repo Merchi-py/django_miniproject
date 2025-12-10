11717534c98dbdc4e5ae4022937ad0f38889662a
Svyat <svyat.kostevich@gmail.com>
2025-12-10 21:36:47 +0100

    feat(forms): added Widgets

--- mini_project/course/forms.py
+++ mini_project/course/forms.py
@@ -1,10 +1,29 @@
 from django import forms
+from django.forms import TextInput, EmailInput
 from .models import Order, Reviews
 
 class OrderCreationForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ["name", "phone", "email"]
+
+        widgets = {
+            "name": forms.TextInput(attrs={
+                "class": "form_modal",
+                "placeholder": "Your name..."
+            }),
+            "phone": TextInput(attrs={
+                "class": "form_modal",
+                "placeholder": "Your Phone Number...",
+                "label": None
+            }),
+            "email": EmailInput(attrs={
+                "class": "form_modal",
+                "placeholder": "Your email..."
+            })
+
+        }
+
 
 class ReviewCreationForm(forms.ModelForm):
     class Meta:
